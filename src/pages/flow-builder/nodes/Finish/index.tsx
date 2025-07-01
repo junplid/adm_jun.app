64656116352d4d34a5ef7e6417aa9093fafe0e84
Junplid <ousadojunplid@gmail.com>
2025-07-01 17:40:20 -0300

    up

--- src/pages/flow-builder/nodes/Finish/index.tsx
+++ src/pages/flow-builder/nodes/Finish/index.tsx
@@ -1,21 +1,17 @@
-import { Node, Position } from "@xyflow/react";
+import { Handle, Node, Position } from "@xyflow/react";
 import { PatternNode } from "../Pattern";
 import { VscDebugStop } from "react-icons/vsc";
-import { CustomHandle } from "../../customs/node";
 
-export const NodeFinish: React.FC<Node> = ({ id }) => {
+export const NodeFinish: React.FC<Node> = () => {
   return (
     <PatternNode.PatternContainer descriptionNode="Finaliza" nameNode="fluxo">
       <div className="">
         <VscDebugStop className="dark:text-zinc-100 text-zinc-800" size={35} />
       </div>
-      <CustomHandle
-        nodeId={id}
-        handleId="main"
-        position={Position.Right}
-        type="source"
-        style={{ right: -8, top: "calc(50% + 3px)" }}
-        isConnectable={true}
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ left: -8, top: 26.5 }}
       />
     </PatternNode.PatternContainer>
   );
