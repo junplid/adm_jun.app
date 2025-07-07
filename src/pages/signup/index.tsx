0f0b2aab862fdb505217789e41806b50f1388736
Junplid <ousadojunplid@gmail.com>
2025-07-07 03:36:56 -0300

    up

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -56,10 +56,9 @@ const FormSchema = z.object({
 
 type Fields = z.infer<typeof FormSchema>;
 
+console.log("STRIPE KEY →", import.meta.env.VITE_STRIPE_PUBLIC_TOKEN);
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_TOKEN);
 export const SignupPage: React.FC = (): JSX.Element => {
-  console.log("STRIPE KEY →", import.meta.env.VITE_STRIPE_PUBLIC_TOKEN);
-  const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_TOKEN);
-
   return (
     <Elements stripe={stripePromise}>
       <FormSignup />
