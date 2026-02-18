bfe4febc1e5dc33131e5d1c872c800869647ae73
junplid <ousadojunplid@gmail.com>
2026-02-18 14:17:04 -0300

    add seção de suporte ao usuario em configuração de conta

--- src/pages/settings/account/index.tsx
+++ src/pages/settings/account/index.tsx
@@ -2,6 +2,7 @@ import { JSX, useContext } from "react";
 import { LayoutSettingsPageContext } from "../contexts";
 import { SectionChangePassword } from "./sections/change-password";
 import { SectionCloseAccount } from "./sections/close-account";
+import { SectionUserSupport } from "./sections/user-support";
 
 export const SettingsAccountPage: React.FC = (): JSX.Element => {
   const { ToggleMenu } = useContext(LayoutSettingsPageContext);
@@ -16,10 +17,25 @@ export const SettingsAccountPage: React.FC = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div className="pl-0.5 grid pr-2 gap-y-5 overflow-y-auto h-[calc(100vh-140px)] pb-7 sm:h-[calc(100vh-175px)] md:h-[calc(100vh-150px)]">
+      <div className="pl-0.5 grid pr-2 gap-y-6 overflow-y-auto h-[calc(100vh-140px)] pb-20 sm:h-[calc(100vh-175px)] md:h-[calc(100vh-150px)]">
         <SectionChangePassword />
         <div className="bg-white/10 w-full h-px"></div>
+        <SectionUserSupport />
+        <div className="bg-white/10 w-full h-px"></div>
         <SectionCloseAccount />
+        <div className="mt-5">
+          <p className="text-sm text-white/70">
+            A sua conta está sujeita aos nossos{" "}
+            <a href="/terms-of-use" className="text-white underline">
+              termos de uso
+            </a>{" "}
+            e{" "}
+            <a href="/terms-of-use" className="text-white underline">
+              política de privacidade
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
