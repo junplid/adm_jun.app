0838a716feb149b68f4bb70826544ed173b6b86d
junplid <ousadojunplid@gmail.com>
2026-02-18 14:16:03 -0300

    delete paginas de teste

--- src/pages/teste1/index.tsx
+++
@@ -1,5 +0,0 @@
-import { JSX } from "react";
-
-export const Teste1Page: React.FC = (): JSX.Element => {
-  return <div className="h-full gap-y-2 flex flex-col">teste1</div>;
-};
--- src/pages/teste2/index.tsx
+++
@@ -1,5 +0,0 @@
-import { JSX } from "react";
-
-export const Teste2Page: React.FC = (): JSX.Element => {
-  return <div className="h-full gap-y-2 flex flex-col">teste2</div>;
-};
